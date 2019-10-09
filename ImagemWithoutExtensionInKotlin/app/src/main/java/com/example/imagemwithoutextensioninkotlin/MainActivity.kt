2fc32285890b86dfa1a66ad5faa11a84678dcc8a
Carlos Maia <carlos.marques89@gmail.com>
2019-10-09 08:54:05 -0400

    Importando Glidder

--- ImagemWithoutExtensionInKotlin/app/src/main/java/com/example/imagemwithoutextensioninkotlin/MainActivity.kt
+++ ImagemWithoutExtensionInKotlin/app/src/main/java/com/example/imagemwithoutextensioninkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.imagemwithoutextensioninkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,7 +20,9 @@ class MainActivity : AppCompatActivity() {
         //Picasso.get().load("https://i.imgur.com/H981AN7.jpg").into(myImageView)
 
         //Não funciona
-        Picasso.get().load("http://d1s5a4e3za7rni.cloudfront.net/Custom/Content/Products/10/13/1013450_purificador-de-agua-electrolux-pc41x-prata_s4_636822411415208179").into(myImageView)
+        //Picasso.get().load("http://d1s5a4e3za7rni.cloudfront.net/Custom/Content/Products/10/13/1013450_purificador-de-agua-electrolux-pc41x-prata_s4_636822411415208179").into(myImageView)
+
+        Glide.with(this).load("http://d1s5a4e3za7rni.cloudfront.net/Custom/Content/Products/10/13/1013450_purificador-de-agua-electrolux-pc41x-prata_s4_636822411415208179").into(myImageView)
 
     }
 }
