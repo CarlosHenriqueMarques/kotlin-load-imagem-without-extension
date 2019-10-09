a4803cd9c3f11d77cdb2446ffa8ce46900d96e99
Carlos Maia <carlos.marques89@gmail.com>
2019-10-09 09:00:03 -0400

    Limpando código não utilizado

--- ImagemWithoutExtensionInKotlin/app/src/main/java/com/example/imagemwithoutextensioninkotlin/MainActivity.kt
+++ ImagemWithoutExtensionInKotlin/app/src/main/java/com/example/imagemwithoutextensioninkotlin/MainActivity.kt
@@ -4,23 +4,13 @@ package com.example.imagemwithoutextensioninkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        //Funciona
-        //Picasso.get().load("https://i.imgur.com/H981AN7.jpg").into(myImageView)
-
-        //Não funciona
-        //Picasso.get().load("http://d1s5a4e3za7rni.cloudfront.net/Custom/Content/Products/10/13/1013450_purificador-de-agua-electrolux-pc41x-prata_s4_636822411415208179").into(myImageView)
 
         Glide.with(this).load("http://d1s5a4e3za7rni.cloudfront.net/Custom/Content/Products/10/13/1013450_purificador-de-agua-electrolux-pc41x-prata_s4_636822411415208179").into(myImageView)
 
